3778bd60359c87625a16029c21095bbf28759c11
dkkdark <49618961+dkkdark@users.noreply.github.com>
2023-01-09 11:42:29 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -5,13 +5,7 @@ const (
 	DBConnectionType = "postgres"
 	HTTPPort         = "80"
 	PrivateKey       = `-----BEGIN RSA PRIVATE KEY-----
-MIIBOQIBAAJAZSOF9iuJYCxwbUSwFoNteC+Z0rifXvvhJK5NghtimuJmD5xfwySL
-CwXhraKfXEUtz+T6XXA2Rp1tY+pVq+FHwQIDAQABAkAPwNi830smj8VzP5+t4grL
-DZ8IE3m/cbw/2mZ4PYu+VBJ1YjzKecM/HSqq4mvQH8KgGQ02x/3f2MgJ+5Eadw+B
-AiEAvuQGRduC8BeuIfiIHvcOw9rUJaN2DyHmHKYC+4q8zO8CIQCHoqQXLa+0zzg0
-q/KcBJ8SfMxIsWuXTEX2yqdGURaWTwIgaZj+l1ptPp/65jP0KR0Gf/Xn8cJRJuHb
-x/FWKQyAkOUCIQCDy9Rq+Wfc5+aTt+mdFRiFXGMc19nWQLVTZAQ63Zx3HQIgCQzr
-TRtfs3Ax22LNLz2blixObO7FwoV1oC/5ovr6FJE=
+## KEY ##
 -----END RSA PRIVATE KEY-----`
 
 	PublicKey = `-----BEGIN PUBLIC KEY-----
